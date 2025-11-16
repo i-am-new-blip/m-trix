7788f83872d47b18ab7250a1d26591bc6e5b56d3
i-am-new-blip <newaccount123321333@proton.me>
2025-11-16 13:06:50 -0300

    Fix syntax errors and clean up code in set.py
    Mess.

--- set.py
+++ set.py
@@ -1,7 +1,6 @@
 """This project is sponsored by: Youtube, Google (Child of the universe), Github, Roblox, Minecraft, Mojang, Micro-soft
 Nintendo, Blackrock, Xbox (Child of Micro-Soft), PlayStation, Betano, Betnacional, The FIFA 2026 World Cup, The FIFA 2022 World Cup
-Honkai Starrail, Genshin Impact, China, USA, Brazil, USSR, IShowSpeed, Python, BASIC, my death, my dead grandma, my dad, my mom and finally
-po- The Agency of Health Care of The DEATH"""
+Honkai Starrail, Genshin Impact, China, USA, Brazil, USSR, IShowSpeed, Python, BASIC, my death, my dead grandma, my dad, my mom and finally The Agency of Health Care of The DEATH"""
 class t:
     def __matmul__(x,n):setattr(x,'t',[]) if not getattr(x,'t',0) else None;x.t.append(n);return x
 class a:
@@ -9,7 +8,7 @@ class a:
 class s:
     def __matmul__(x,n):n.t[0].n-=n.t[1].n;return n.t[0]
 class b:
-    def __sub__(x,n):x.n=n-(x.n//4)if n>15812 else n+(x.n*4)
+    def __sub__(x,n):x.n=n-(x.n//4)if n>15812 else x.n=n+(x.n*4)
     def __truediv__(x,n):x.n//=(x.n*n^72);x*x.n
     def __mul__(x,n):x.n*=n;x.n//=(7^(n-(n^2)));x-(x.n+4)
     def __matmul__(x,n):x.n=s()@(t()@n()@n())
@@ -23,7 +22,20 @@ class m:
     def __matmul__(x,n):n.t[0].n*=n.t[1].n;return n.t[0]
 class n:
     def __matmul__(x,n):x.n=1;return x
+class l:
+    def __matmul__(x,n):return getattr(__builtins__,n)
 class x:
-    def __matmul__(x,_):getattr(__builtins__,c()@(t()@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(s()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(s()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(s()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(n()@n)))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))))(_)
-    #                   If you setuped correctly and you are not running Python in one of these devices: Android, IOS, Microsoft Windows 1, Microsoft Windows 1.1, Micro-soft Windows 2, Micro-soft windows 97, Micro-soft Window 95, Microsoft Windows ME, an Samsung Smart refrigerator, an Commodore 64, an Macintosh from the 80s, an dead rat, a device that can run bad apple, my life, your mom is so cool, Android with the programming capatibility layer, MacOS, Linux, Ubuntu, Kali Linux, Fedora Linux, A mc donalds kiosk it should be the coolest cool cooly cooly austy busty custy dusty eusty fusty gusty husty iusty justy kusty lusty musty nusty osuty psuty rsuty wsuty xsuty ysuty zsuty and also you have to save this file in UTF-8 because look at my time bombs!😀 😃 😄 😁 😆 😅 😂 🤣 🥲 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 😞 😔 😟 😕 🙁 ☹️ 😣 😖 😫 😩 🥺 😢 😭 😤 😠 😡 🤬 🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🤗 🤔 🤭 🤫 🤥 😶 😐 😑 😬 🙄 😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 🤐 🥴 🤢 🤮 🤧 😷 🤒 🤕 🤑 🤠 😈 👿 👹 👺 🤡 💩 👻 💀 ☠️ 👽 👾 🤖 🎃 😺 😸 😹 😻 😼 😽 🙀 😿 😾 Ge 👋 🤚 🖐 ✋ 🖖 👌 🤌 🤏 ✌️ 🤞 🤟 🤘 🤙 👈 👉 👆 🖕 👇 ☝️ 👍 👎 ✊ 👊 🤛 🤜 👏 🙌 👐 🤲 🤝 🙏 ✍️ 🤳 💪 🦾 🦵 🦿 🦶 👣 👂 🦻 👃 🫀 🫁 🧠 🦷 🦴 👀 👁 👅 👄 💋 🩸look now im brazilian! okay so lemme tell this straight, it evaluates eval(_)
-  # What diddy blud is doing on the calculator? Is blud, Eistein??
+    def __matmul__(x,_):return l()@(c()@(t()@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(s()@(t()@(n()@n)@(n()@n)))))@(
+        e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(
+        t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(
+        n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(
+        n()@n)@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)
+        ))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))@(e()@(t(
+        )@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))@(a()@(t()@(
+        e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(s()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(
+        a()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n
+        )@(n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n
+        )))))))@(a()@(t()@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(s()@(t()@(n()@n)@(n()@n)))))@(e()@(t()@(a()@(t()@(n(
+        )@n)@(n()@n)))@(n()@n)))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(
+        n()@n)))))@(e()@(t()@(a()@(t()@(n()@n)@(n()@n)))@(a()@(t()@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)@(n()@n)))))))))(_)
+        # eval(_)
